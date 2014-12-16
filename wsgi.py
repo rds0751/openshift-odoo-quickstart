28b7cda64556f34189d3b99776bce5fd6a99a24a
Amon-Ra <amon.raj@gmail.com>
2014-12-16 01:04:33 +0100

    Tested 8.0 branch multiple patches added.
    Old version does not working. Updated all source code and added
    post_deploy hook to init database.
    Need more testing.

--- wsgi.py
+++ wsgi.py
@@ -15,25 +15,31 @@ except IOError:
 
 import sys
 
-ODOO_ROOT_DIR = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR', '.'), 'odoo')
+ODOO_ROOT_DIR = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', '.'), 'odoo')
 if ODOO_ROOT_DIR not in sys.path:
     sys.path.append(ODOO_ROOT_DIR)
+
+os.environ['XDG_DATA_HOME'] = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', '.'),'.local/share')
+os.environ['XDG_CACHE_HOME'] = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', '.'),'.cache')
+os.environ['XDG_CONFIG_HOME'] = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', '.'),'.config')
+
 
 import openerp
 
 #----------------------------------------------------------
 # Common
 #----------------------------------------------------------
-odoo.multi_process = True # Nah!
+openerp.multi_process = True # Nah!
 
 # Equivalent of --load command-line option
-odoo.conf.server_wide_modules = ['web']
-conf = odoo.tools.config
+openerp.conf.server_wide_modules = ['web']
+conf = openerp.tools.config
 
 # Path to the ODOO Addons repository (comma-separated for
 # multiple locations)
 
-conf['addons_path'] = os.path.join(ODOO_ROOT_DIR, 'openerp/addons')
+conf['addons_path'] = ','.join([os.path.join(ODOO_ROOT_DIR,'openerp' ,'addons'),os.path.join(ODOO_ROOT_DIR, 'addons'),os.path.join(os.environ.get('OPENSHIFT_REPO_DIR', '.'), 'addons')])
+#conf['addons_path'] = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR', '.'), 'addons')
 
 # Optional database config if not using local socket
 conf['db_host']     = os.environ['OPENSHIFT_POSTGRESQL_DB_HOST']
@@ -42,17 +48,20 @@ conf['db_user']     = os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME']
 conf['db_name']     = os.environ['OPENSHIFT_APP_NAME']
 conf['db_password'] = os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD']
 
+conf['logfile'] = os.path.join($OPENSHIFT_LOG_DIR , 'odoo.log')
+
 #----------------------------------------------------------
 # Generic WSGI handlers application
 #----------------------------------------------------------
 application = openerp.service.wsgi_server.application
+openerp.service.server.load_server_wide_modules()
 
 #----------------------------------------------------------
 # Gunicorn
 #----------------------------------------------------------
 # Standard ODOO XML-RPC port is 8069
 #bind = '127.0.0.1:8069'
-#pidfile = '.gunicorn.pid'
+#pidfile = ODOO_ROOT_DIR+'.gunicorn.pid'
 #workers = 4
 #timeout = 240
 #max_requests = 2000
@@ -60,10 +69,10 @@ application = openerp.service.wsgi_server.application
 #
 # Below for testing only
 #
-if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
-    # Wait for a single request, serve it and quit.
-    httpd.handle_request()
+#if __name__ == '__main__':
+#    from wsgiref.simple_server import make_server
+#    httpd = make_server('localhost', 8051, application)
+#    # Wait for a single request, serve it and quit.
+#    httpd.handle_request()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
